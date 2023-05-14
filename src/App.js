8cae8375452665d1f1cbceecf8c82f3edb44bcdc
hemanth <applicationcomputer257@gmail.com>
2023-05-14 23:04:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/Nav";
+import Filter from "./components/Filter";
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Filter />
+    </>
+  );
+};
+
+export default App;
